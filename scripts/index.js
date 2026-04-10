89f02b49eda8eeddb6fa0cda045592f835d85bc5
[200~uu22347~ <exopxi56@gmail.com>
2026-04-10 17:50:04 +0900

    수정

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const bestItem = document.querySelector('.best_slide');
 const giftItem = document.querySelector('.gift_slide');
 const disneyItem = document.querySelector('.disney_product');
 const vanItem = document.querySelector('.van_product');
-console.log(bestItem,giftItem);
+console.log(bestItem,giftItem,disneyItem,vanItem);
 
 const bestSwiper = new Swiper(bestItem,{
     slidesPerView:2,
@@ -14,14 +14,16 @@ const giftSwiper = new Swiper(giftItem,{
     spaceBetween:35,
     loop:true,
     centeredSlides:true,
+    navigation:{
+        nextEl:'.gift_slide .next',
+        prevEl:'.gift_slide .prev',
+    }
 })
 const disneySwiper = new Swiper(disneyItem,{
     slidesPerView:4,
-    spaceBetween:30,
     loop:true,
 })
 const vanSwiper = new Swiper(vanItem,{
     slidesPerView:4,
-    spaceBetween:30,
     loop:true,
 })
